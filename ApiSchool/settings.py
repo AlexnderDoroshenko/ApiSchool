e9cac374c3af6bbe1de5af10cee4ce7297349ad3
AlexnderDoroshenko <doroshenkoaldm@gmail.ru>
2021-12-14 17:02:54 +0200

    add random secret_key

--- ApiSchool/settings.py
+++ ApiSchool/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import socket
+import string
+import random # define the random module
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_HOSTNAME = socket.gethostname()
@@ -23,7 +26,8 @@ SERVER_IP = "0.0.0.0"
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = ''.join(random.choices(
+    string.ascii_uppercase + string.digits, k=13))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
